3ec408f74cbbf1e18ffc3ea5a9f6d047dc90501e
Esin Çağla KIRAL <caglakiral@gmail.com>
2022-09-09 03:42:00 +0300

    add remove-popup in customer-address
    Co-Authored-By: Kübra Tanrıkulu <64233769+kubratanrikulu@users.noreply.github.com>

--- src/app/features/customers/pages/customer-address/customer-address.component.ts
+++ src/app/features/customers/pages/customer-address/customer-address.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Address } from '../../models/address';
+import { MessageService } from 'primeng/api';
+import { Customer } from '../../models/customer';
 import { CustomersService } from '../../services/customer/customers.service';
 
 @Component({
@@ -10,16 +12,44 @@ import { CustomersService } from '../../services/customer/customers.service';
 export class CustomerAddressComponent implements OnInit {
   selectedCustomerId!: number;
   customerAddress: Address[] = [];
+  customer!: Customer;
+  addressToDelete!: Address;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private customerService: CustomersService,
-    private router: Router
+    private router: Router,
+    private messageService:MessageService
   ) {}
 
   ngOnInit(): void {
     this.getCustomerById();
+    this.messageService.clearObserver.subscribe((data) => {
+      if (data == 'reject') {
+        this.messageService.clear();
+      } else if (data == 'confirm') {
+        this.messageService.clear();
+        this.remove();
+      }
+    });
   }
+
+  // getCustomerById() {
+  //   this.activatedRoute.params.subscribe((params) => {
+  //     if (params['id']) this.selectedCustomerId = params['id'];
+  //   });
+  //   if (this.selectedCustomerId == undefined) {
+  //     //toast
+  //   } else {
+  //     this.customerService
+  //       .getCustomerById(this.selectedCustomerId)
+  //       .subscribe((data) => {
+  //         data.addresses?.forEach((adress) => {
+  //           this.customerAddress.push(adress);
+  //         });
+  //       });
+  //   }
+  // }
 
   getCustomerById() {
     this.activatedRoute.params.subscribe((params) => {
@@ -31,6 +61,8 @@ export class CustomerAddressComponent implements OnInit {
       this.customerService
         .getCustomerById(this.selectedCustomerId)
         .subscribe((data) => {
+          this.customer = data;
+          this.customerAddress = []
           data.addresses?.forEach((adress) => {
             this.customerAddress.push(adress);
           });
@@ -49,4 +81,23 @@ export class CustomerAddressComponent implements OnInit {
       `/dashboard/customers/${this.selectedCustomerId}/address/update/${addressId}`
     );
   }
+
+  removePopup(address: Address) {
+    this.addressToDelete = address;
+    this.messageService.add({
+      key: 'c',
+      sticky: true,
+      severity: 'warn',
+      detail: 'Are you sure you want to delete?',
+    });
+  }
+
+  remove() {
+    this.customerService
+      .removeAddress(this.addressToDelete, this.customer)
+      .subscribe((data) => {
+        // this.getCustomerById();
+      });
+  }
+
 }
